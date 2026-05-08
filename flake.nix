7ab444f5fcfca614f5db6f8d3f54e6e5446c961e
delta-psi <deltapsi.exe@proton.me>
2026-05-08 17:44:16 -0400

    Edited desc in flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very customizable SDDM theme that actually looks good";
+  description = "A (fork of a) very customizable SDDM theme that actually looks good";
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
   outputs = {
